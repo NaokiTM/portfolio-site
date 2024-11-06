8f19b5275d872b30b68d72a474e5c1067623e113
NaokiTM <ntsome21@gmail.com>
2024-11-06 18:23:53 +0000

    rid of css emoji

--- src/app/components/Hero.tsx
+++ src/app/components/Hero.tsx
@@ -4,22 +4,21 @@ import Emoji from './Emoji'
 
 export default function Hero() {
   return (
-    <div className='flex flex-row'>
-        <div className='flex flex-col'>
-            <div >
-                <div className='enormous-text mb-8 pl-2 font-medium tracking-tighter leading-none'>
-                    <div className='-mb-4'>Naoki</div>
-                    <div>Miyagawa</div>
-                </div>
-                <div className={`${instrumentserif.className} pl-5 text-5xl`}>
-                    <div>I am a computer science student </div>
-                    <div>passionate about (building) beautiful websites</div>
-                    <div>and creating apps to solve fun problems.</div>
+    <div className='flex justify-center items-center p-8'>
+        <div className='flex flex-row'>
+            <div className='flex flex-col'>
+                <div >
+                    <div className='mb-8 pl-2 font-medium tracking-tighter leading-none text-6xl'>
+                        <div className='-mb-4'>Naoki</div>
+                        <div>Miyagawa</div>
+                    </div>
+                    <div className={`${instrumentserif.className} pl-5 text-5xl`}>
+                        <div>I am a computer science student </div>
+                        <div>passionate about (building) beautiful websites</div>
+                        <div>and creating apps to solve fun problems.</div>
+                    </div>
                 </div>
             </div>
-        </div>
-        <div className='flex flex-grow items-center justify-center'>
-            <Emoji />
         </div>
     </div>
   )
