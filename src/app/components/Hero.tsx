f547d363f20ba26ebb2b72e5059e260fce5017b0
NaokiTM <ntsome21@gmail.com>
2024-11-06 18:59:14 +0000

    more formatting

--- src/app/components/Hero.tsx
+++ src/app/components/Hero.tsx
@@ -1,22 +1,19 @@
 import React from 'react'
 import { instrumentserif } from '../layout'
-import Emoji from './Emoji'
 
 export default function Hero() {
   return (
     <div className='flex justify-center items-center p-8'>
         <div className='flex flex-row'>
             <div className='flex flex-col'>
-                <div >
-                    <div className='mb-3 font-medium tracking-tighter leading-none text-6xl'>
-                        <div className='-mb-2'>Naoki</div>
-                        <div>Miyagawa</div>
-                    </div>
-                    <div className={`${instrumentserif.className} pl-1 text-3xl`}>
-                        <div>I am a computer science student </div>
-                        <div>passionate about (building) beautiful websites</div>
-                        <div>and creating apps to solve fun problems.</div>
-                    </div>
+                <div className={`${instrumentserif.className} font-medium text-8xl mb-10 pt-8 pr-8 pb-8`}>
+                    <div className=''>Naoki</div>
+                    <div>Miyagawa</div>
+                </div>
+                <div className='text-3xl leading-none tracking-tighter font-medium'>
+                    <div>I am a computer science student </div>
+                    <div>passionate about (building) beautiful websites</div>
+                    <div>and creating apps to solve fun problems.</div>
                 </div>
             </div>
         </div>
