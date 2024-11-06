00c0c8f79587ac875d0d52c00081d9e9a1dfe48a
NaokiTM <ntsome21@gmail.com>
2024-11-06 19:47:28 +0000

    fixed issue with fading-in-out during rerender of page

--- src/app/components/ThemeWrapper.tsx
+++ src/app/components/ThemeWrapper.tsx
@@ -6,14 +6,16 @@ import { FaRegMoon } from "react-icons/fa";
 
 export default function ThemeWrapper({children, }: Readonly<{ children: React.ReactNode; }>) {
   const [isDarkMode, setIsDarkMode] = useState(false);
+  const [animate, setAnimate] = useState(false);
 
   const toggleDarkMode = () => {
+    setAnimate(true);
     setIsDarkMode((prevMode) => !prevMode);  //prevmode ideal to correctly keep track of asynchronous state toggling 
   };
 
   return (
     <>
-        <div className={isDarkMode ? "toLightMode bg-white text-black" : "toDarkMode bg-black text-white"}>
+        <div className={`${isDarkMode ? "bg-black text-white" : "bg-white text-black"} ${animate ? (isDarkMode ? "toDarkMode" : "toLightMode") : ""}`}>
             {children}
             <div className='w-100 flex justify-end sticky bottom-0 bg-opacity-0'> 
                 <button className='p-4 bg-opacity-0' onClick={toggleDarkMode}><FaRegMoon className='h-[25px] w-[25px]'/></button>
