f0648d91b599310a71644a7759f71ac7176ee246
NaokiTM <ntsome21@gmail.com>
2024-11-08 16:05:21 +0000

    projects are now aligned as a row instead of column

--- src/app/components/content/Skillset.tsx
+++ src/app/components/content/Skillset.tsx
@@ -4,7 +4,6 @@ export default function Skillset() {
   return (
     <div className=''>
       <div className={`leading-none tracking-tighter font-semibold text-7xl pb-8`}>Skillset: </div>
-
     </div>
   )
 }
