d38fb9a98887b83a1c2f31dde6eaccf7ff000060
NaokiTM <ntsome21@gmail.com>
2024-11-09 03:52:23 +0000

    edited assetPrefix path

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   basePath: '/portfolio-site',
-  assetPrefix: '/portfolio-site',
+  assetPrefix: '/portfolio-site/public',
   output: "export",
   reactStrictMode: true,
 };
