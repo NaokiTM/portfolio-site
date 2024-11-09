235578f302f6a142c1f944429841682a3de12e6b
NaokiTM <ntsome21@gmail.com>
2024-11-09 21:49:08 +0000

    removed svg connections for now, and added instrumentserif to my projects section

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,6 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  dangerouslyAllowSVG: true,
   basePath: '/portfolio-site',
   assetPrefix: '/portfolio-site/public',
   output: "export",
--- src/app/components/content/Projects.tsx
+++ src/app/components/content/Projects.tsx
@@ -1,4 +1,11 @@
 import React from 'react'
+import { Instrument_Serif } from 'next/font/google';
+
+const instrumentSerif = Instrument_Serif({
+  subsets: ['latin'],
+  weight: ['400'],   
+  style: ['italic'],
+});
 
 export default function Projects() {
   return (
@@ -6,17 +13,17 @@ export default function Projects() {
         <div className={`leading-none tracking-tighter font-semibold text-7xl pb-8`}>Projects:</div>
         <div className='flex flex-row align-top space-x-4'>
           <div className='flex flex-col'>
-            <div className={`instrument-serif-italic text-4xl mb-2`}>Personal Website</div>
+            <div className={`${instrumentSerif.className} text-4xl mb-2`}>Personal Website</div>
             <p>This was one of the more fun projects I got to build, since I get to build a site that has layouts, typography, and features that I personally like. This project was a way for me to get hands on experience with contexts and refs, both needed for the theme selector and scroll-to-component features. The positioning of the sticky sidebar was something that took trial and error, but taught me more about how different utility classes in tailwind work together or against each other. </p>
           </div>
 
           <div className='flex flex-col'>
-            <div className={`instrument-serif-italic text-4xl mb-2`}>Knights Tour Autosolver</div>
+            <div className={`${instrumentSerif.className} text-4xl mb-2`}>Knights Tour Autosolver</div>
             <p>An implementation of the Knights Tour puzzle, which I solve by using warnsdorffs rule (basically finds the move that has the smallest number of moves from it on the board). This rule was implemented by using nested loops to loop through each of the possible movesets, and iterating through the number of moves possible from that square.</p>
           </div>
 
           <div className='flex flex-col'>
-            <div className={`instrument-serif-italic text-4xl mb-2`}>React Typing Game</div>
+            <div className={`${instrumentSerif.className} text-4xl mb-2`}>React Typing Game</div>
             <p className='mb-8'>The project is inspired by monkeytype, which is different to other typing tests, in that the test doesn&apos;t take place in a input field, but rather through splitting the words into an array of letters, and moving the caret from one letter to another. This is more visually appealing, and allows for individual letter highlighting to indicate the state of correctness for each letter.</p>
           </div>
         </div>
