62d257d1a0246449f1413497162d69ff92873871
naoki <ntsome21@gmail.com>
2025-09-07 01:11:56 +0000

    fixed order of screenshots in project cards

--- src/app/data/ProjectsContent.tsx
+++ src/app/data/ProjectsContent.tsx
@@ -7,7 +7,7 @@ export const projects = [
     title: "Personal Website",
     icon: (<FiUser className= 'text-4xl mb-2' />),
     ghlink: "https://github.com/NaokiTM/portfolio-site",
-    bgscreen: "/projectscreenshots/typing.png",
+    bgscreen: "/projectscreenshots/portfolio.png",
     description: (
       <>
         This was one of the more fun projects I got to build, since I get to build a site that has layouts, typography, and features that I personally like. This project was a way for me to get hands on experience with contexts and refs, both needed for the theme selector and scroll-to-component features. The positioning of the sticky sidebar was something that took trial and error, but taught me more about how different utility classes in tailwind work together or against each other. 
@@ -29,7 +29,7 @@ export const projects = [
     title: "React Typing Game",
     icon: (<BsKeyboard className = 'text-4xl mb-2' />),
     ghlink: "https://github.com/NaokiTM/TS-typing-game",
-    bgscreen: "/projectscreenshots/portfolio.png",
+    bgscreen: "/projectscreenshots/typing.png",
     description: (
       <>
        The project is inspired by monkeytype, which is different to other typing tests, in that the test doesn&apos;t take place in a input field, but rather through splitting the words into an array of letters, and moving the caret from one letter to another. This is more visually appealing, and allows for individual letter highlighting to indicate the state of correctness for each letter.
